550ae2dd4c93aafa7c4dd52842704098364f5279
Shawn Lin <jallen0927@hotmail.com>
2015-06-29 23:40:11 +1200

    first commit with customised menu

---
+++ public/modules/core/controllers/header.client.controller.js
@@ -0,0 +1,50 @@
+'use strict';
+
+angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
+	function($scope, Authentication, Menus) {
+		$scope.authentication = Authentication;
+		$scope.isCollapsed = false;
+//		$scope.menu = Menus.getMenu('topbar');
+
+		$scope.toggleCollapsibleMenu = function() {
+			$scope.isCollapsed = !$scope.isCollapsed;
+		};
+
+		// Collapsing the menu after navigation
+		$scope.$on('$stateChangeSuccess', function() {
+			$scope.isCollapsed = false;
+		});
+
+        $scope.menu = getMenu();
+
+        function getMenu () {
+            var menuName = 'main-menu',
+                menuData = [
+                    {
+                        'title': 'HOME',
+                        'url': '/'
+                    },
+                    {
+                        'title': 'ABOUT',
+                        'url': 'about'
+                    },
+                    {
+                        'title': 'PRODUCTS & SERVICE',
+                        'url': 'products-service'
+                    },
+                    {
+                        'title': 'CONTACT',
+                        'url': 'contact'
+                    }
+                ],
+                menu = Menus.addMenu(menuName, true);
+                angular.forEach(menuData, function(item, key) {
+                    Menus.addMenuItem(menuName, item.title, item.url);
+                });
+
+            return menu;
+        };
+
+
+	}
+]);
